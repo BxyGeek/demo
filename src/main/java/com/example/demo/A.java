5b141135597b8c92f6d17b890fc959dc72460561
buxy <bxy_xiaoyu@163.com>
2019-11-07 10:22:50 +0800

    this is init

---
+++ src/main/java/com/example/demo/A.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * 类描述信息 ()
+ *
+ * @author bxy
+ * @Date 2019/11/7$ 10:18$
+ * @version: V_1.0.0
+ */
+public class A {
+}
---
+++ src/main/java/com/example/demo/B.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * 类描述信息 ()
+ *
+ * @author bxy
+ * @Date 2019/11/7$ 10:19$
+ * @version: V_1.0.0
+ */
+public class B {
+}
